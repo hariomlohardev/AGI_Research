7be160ade49ecafb41d074acb41703b0ce11f08d
hariomloharapps <hariomlohar.new@gmail.com>
2026-07-04 07:14:57 +0530

    day 4 all things completed

--- Stage_00_Visual_Math/Day_4_elemination/day4_elimination.py
+++ Stage_00_Visual_Math/Day_4_elemination/day4_elimination.py
@@ -9,7 +9,7 @@ b = np.array([1, -2, 7])
 
 # 2. Solve instantly
 x = np.linalg.solve(A, b)
-print(f"The solution [x, y, z] is: {x}")
+# print(f"The solution [x, y, z] is: {x}")
 
 result = []
 col_1 = A[:,0]
@@ -18,4 +18,15 @@ col_3 = A[:,2]
 
 combination = (x[0]*col_1) +(x[1]*col_2) + (x[2]*col_3)
 
-print(combination)
+# print(combination)
+
+from scipy.linalg import null_space
+
+A = np.array([[1, 2],
+              [3, 6]])
+
+subspace = null_space(A)
+
+result = A @ subspace
+
+print(result)
